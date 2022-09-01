d4bd5a917978dc88f8fb400bab88fa6ea85f722e
ShamievDima <90345143+ShamievDima@users.noreply.github.com>
2022-09-02 00:28:23 +0500

    Изменены настройки БД

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': os.getenv(
             'DB_ENGINE',
-            default='django.db.backends.postgresql'
+            default='django.db.backends.postgresql_psycopg2'
         ),
         'NAME': os.getenv('DB_NAME', default='foodgram'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
